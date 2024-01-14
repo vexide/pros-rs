e16e42ae3188f4b65dbcb7f1fc5d3e2489828fcb
Gavin-Niederman <gavinniederman@gmail.com>
2024-01-13 16:18:59 -0800

    fix: revert basic example

--- pros/examples/basic.rs
+++ pros/examples/basic.rs
@@ -1,41 +1,16 @@
 #![no_std]
 #![no_main]
 
-extern crate alloc;
-
-use core::time::Duration;
-
-use alloc::sync::Arc;
-use pros::{prelude::*, sync::Mutex, task::delay};
-use spin::mutex;
+use pros::prelude::*;
 
 #[derive(Default)]
 pub struct Robot;
 #[async_trait]
-impl SyncRobot for Robot {
-    fn opcontrol(&mut self) -> pros::Result {
+impl AsyncRobot for Robot {
+    async fn opcontrol(&mut self) -> pros::Result {
         println!("basic example");
-
-        let mutex = Arc::new(Mutex::new(0u32));
-
-        pros::task::spawn({
-            let mutex = mutex.clone();
-            move || {
-                let mut guard = mutex.lock_blocking().unwrap();
-                println!("mutex: {:?}", mutex);
-                println!("{}", *guard);
-                delay(Duration::from_secs(1));
-                *guard += 1;
-                drop(guard)
-            }
-        });
-
-        let guard = mutex.lock_blocking().unwrap();
-        println!("mutex: {:?}", mutex);
-        println!("{}", *guard);
-        drop(guard);
 
         Ok(())
     }
 }
-sync_robot!(Robot);
+async_robot!(Robot);
