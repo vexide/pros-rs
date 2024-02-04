1c738b35710f60240175316f14f91a748a15e69e
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-02-04 03:31:35 -0600

    refactor: link libpros through pros-sys

--- packages/pros-sys/build.rs
+++ packages/pros-sys/build.rs
@@ -1,12 +1,10 @@
-use cfg_if::cfg_if;
 
 fn main() {
-    cfg_if! {
-        if #[cfg(not(feature = "no-link"))] {
-            let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    if cfg!(not(feature = "no-link")) {
+        let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
 
-            #[cfg(not(feature = "no-link"))]
-            println!("cargo:rustc-link-search=native={manifest_dir}/link");
-        }
+        println!("cargo:rustc-link-search=native={manifest_dir}/link");
+        println!("cargo:rustc-link-lib=static=pros");
+        println!("cargo:rustc-link-lib=static=c");
     }
 }
