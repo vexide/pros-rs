4becedd5b912a3a025ca4ab6fc263241a2b190c5
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-03-02 23:10:16 -0600

    refactor: move all libraries out of target spec

--- packages/pros-sys/build.rs
+++ packages/pros-sys/build.rs
@@ -4,5 +4,7 @@ fn main() {
 
         println!("cargo:rustc-link-search=native={manifest_dir}/link");
         println!("cargo:rustc-link-lib=static=pros");
+        println!("cargo:rustc-link-lib=static=c");
+        println!("cargo:rustc-link-lib=static=gcc");
     }
 }
